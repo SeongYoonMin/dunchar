c1def9d3d658eb0b6212f1bd2d5edac1107cdc10
SeongYoonMin <namo4862@naver.com>
2023-10-27 21:49:38 +0900

    Create: result-view

---
+++ src/views/SearchResultView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const resultId = route.params.charId;
+</script>
+
+<template>
+  <section>
+    <p v-if="resultId">{{ resultId }}</p>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>

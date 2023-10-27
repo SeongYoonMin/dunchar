52306767a533a55e8f9e796fd25e2437fce21204
SeongYoonMin <namo4862@naver.com>
2023-10-27 14:04:40 +0900

    ADD: Search Dynamic route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SearchView from '@/views/SearchView.vue'
+import SearchResultView from '@/views/SearchResultView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,8 +15,12 @@ const router = createRouter({
       path: '/search',
       name: 'search',
       component: SearchView
+    },
+    {
+      path: '/search/:charId',
+      name: 'searchResult',
+      component: SearchResultView
     }
-
   ]
 })
 
